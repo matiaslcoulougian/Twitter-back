aad08f345defb6010b8892b417067ce38e954fd8
Micaela <micaela.dominguez@ing.austral.edu.ar>
2021-09-03 15:57:47 -0300

    Validator and types.ts

--- src/components/user/validator/types.ts
+++ src/components/user/validator/types.ts
@@ -3,13 +3,13 @@ import {date} from "zod";
 export const createUserInputSchema = z.object({
     id: z.number(),
     name: z.string(),
-    bibliography: z.string(),
+    bibliography: z.string().optional(),
     password: z.string(),
     phone:z.string(),
     email: z.string().email('Invalid mail format'),
     userName: z.string(),
-    location: z.string(),
-    webSite: z.string(),
+    location: z.string().optional(),
+    webSite: z.string().optional(),
     birthDay: z.string(),
     createdAt: z.string(),
     isActive: z.boolean(),
@@ -17,14 +17,42 @@ export const createUserInputSchema = z.object({
 export type CreateUserInput = {
     id: number;
     name: string;
-    bibliography: string;
+    bibliography?: string;
     password: string;
     phone: string;
     email: string;
     userName: string;
-    location: string;
-    webSite: string;
+    location?: string;
+    webSite?: string;
     birthDay: string;
     createdAt: string ;
     isActive: boolean;
 };
+export const updateUserInputSchema= z.object({
+    id: z.number().optional(),
+    name: z.string().optional(),
+    bibliography: z.string().optional(),
+    password: z.string().optional(),
+    phone: z.string().optional(),
+    email: z.string().email('Invalid mail format').optional(),
+    userName: z.string().optional(),
+    location: z.string().optional(),
+    webSite: z.string().optional(),
+    birthDay: z.string().optional(),
+    createdAt: z.string().optional(),
+    isActive: z.boolean().optional(),
+})
+export type UpdateUserInput ={
+    id?: number;
+    name?: string;
+    bibliography?: string;
+    password?: string;
+    phone?: string;
+    email?: string;
+    userName?: string;
+    location?: string;
+    webSite?: string;
+    birthDay?: string;
+    createdAt?: string ;
+    isActive?: boolean;
+}
--- src/components/user/validator/user.validator.ts
+++ src/components/user/validator/user.validator.ts
@@ -1,8 +1,20 @@
-import {CreateUserInput, createUserInputSchema} from "@/components/user/validator/types";
+import {
+    CreateUserInput,
+    createUserInputSchema,
+    UpdateUserInput,
+    updateUserInputSchema
+} from "@/components/user/validator/types";
 
 export class UserValidator{
     public static validateCreateUserBody(body: unknown): CreateUserInput{
         const result= createUserInputSchema.safeParse(body);
+        if(!result.success){
+            throw new Error(result.error.message);
+        }
+        return result.data;
+    }
+    public static validateUpdateUserBody(body: unknown): UpdateUserInput{
+        const result = updateUserInputSchema.safeParse(body);
         if(!result.success){
             throw new Error(result.error.message);
         }
